928f0b8a16f6b7bf6cfd1ad94331c971f94151aa
BenamgharAmina <145927059+BenamgharAmina@users.noreply.github.com>
2024-11-01 16:35:44 +0100

    Delete preload.js

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-    addExpense: (name, price) => ipcRenderer.invoke('add-expense', name, price),
-    deleteExpense: (id) => ipcRenderer.invoke('delete-expense', id),
-    getExpenses: () => ipcRenderer.invoke('get-expenses'),
-    loadExpenses: (callback) => ipcRenderer.on('load-expenses', callback)
-});
